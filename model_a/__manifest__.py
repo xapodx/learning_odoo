3d7303fcb4127f1779e2403049d9dbc3814e572a
xapodx <85314055+xapodx@users.noreply.github.com>
2025-02-09 13:29:02 +0300

    new

---
+++ model_a/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Model A",
+    'author': "Abdulrahman",
+    'version': '17.0.0.1',
+    'category': '',
+    'depends': ['base'],
+    'data':[
+
+    ],
+
+    'application': True,
+
+}
---
+++ model_a/models/model_c.py
@@ -0,0 +1,4 @@
+from odoo import models
+
+class Modelc(models.AbstractModel):
+    _name = 'model.c'
