4ce5d08b495aea68904f53e1f288751701d5048c
xapodx <85314055+xapodx@users.noreply.github.com>
2025-02-10 22:39:14 +0300

    task2: the founction creat have been created

--- model_a/models/property.py
+++ model_a/models/property.py
@@ -16,9 +16,21 @@ class property(models.Model):
     bouns = fields.Integer(required=1)
 
 
+    _sql_constraints = [
+        ('unique_name', 'unique(name)', 'Name must be unique!')
+    ]
 
     @api.constrains('bouns')
     def _check_bouns(self):
         for rec in self:
             if rec.bouns == 0:
                 raise ValidationError('the bouns cant be less than 0')
+
+    @api.model_create_multi
+    def create(self,vals):
+        res = super(property, self).create(vals)
+        print("inside")
+        return res
+
+
+    
