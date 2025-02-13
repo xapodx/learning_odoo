37c0e8647a12a0e72ecb418466c89aeb80ad6f4f
xapodx <85314055+xapodx@users.noreply.github.com>
2025-02-13 10:18:16 +0300

    i made the allwonces to be stored aotumatecly when ae chooce the grade and i add a value when the shift box is true for the employees who is working in shift mode

--- model_a/models/hr.py
+++ model_a/models/hr.py
@@ -1,8 +1,22 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    property_id = fields.Many2one('property', string="Property")
-    salary_id = fields.Many2one('salary', string="Salary")
+    salary = fields.Float()
     shift = fields.Boolean(string="Shift", default=False)
+    grade_id = fields.Many2one('property')
+
+    # Using the related field to link allowances to the 'grade_id'
+    medical_allowance = fields.Float(related='grade_id.mediacl', string="Medical")
+    transport_allowance = fields.Float(related='grade_id.transport', string="Transport")
+    housing_allowance = fields.Float(related='grade_id.housing', string="Housing")
+    bouns_allowance = fields.Integer(related='grade_id.bouns', string="Bouns")
+    total = fields.Float(compute='_compute_total', store=True, readonly=1)
+
+    @api.depends('housing_allowance', 'transport_allowance', 'medical_allowance', 'salary', 'shift')
+    def _compute_total(self):
+        for rec in self:
+            rec.total = rec.housing_allowance + rec.medical_allowance + rec.transport_allowance + rec.salary
+            if rec.shift:
+                rec.total += 300  # Add 300 if the shift is True
