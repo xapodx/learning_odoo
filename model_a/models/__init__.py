9662c24e51471806a314470f55a77f88667f319f
xapodx <85314055+xapodx@users.noreply.github.com>
2025-02-13 08:56:07 +0300

    task3: i have finished he task 3 nad added the salary page

--- model_a/models/__init__.py
+++ model_a/models/__init__.py
@@ -2,3 +2,4 @@
 from . import property
 from . import jobs
 from . import hr
+from . import salary_info
--- model_a/models/hr.py
+++ model_a/models/hr.py
@@ -1,10 +1,8 @@
-from odoo import models , fields
+from odoo import models, fields
 
-
-
-class Hr(models.Model):
+class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    property_id = fields.Many2one('property')
-
-
+    property_id = fields.Many2one('property', string="Property")
+    salary_id = fields.Many2one('salary', string="Salary")
+    shift = fields.Boolean(string="Shift", default=False)
