988ae88d9e210a3d7c8a29a24b6f7e583f0c4a24
xapodx <85314055+xapodx@users.noreply.github.com>
2025-02-18 13:56:46 +0300

    task6: the wizard task is done with making the security groups active and translate the module to arabic

--- model_a/wizard/change_state.py
+++ model_a/wizard/change_state.py
@@ -1,16 +1,14 @@
-from odoo import fields,models
-
+from odoo import fields, models
 
 class ChangeState(models.TransientModel):
     _name = 'state'
 
-
-    grade_id = fields.Many2one('grade', string='Grade')
+    grade_id = fields.Many2one('grade', string='Grade', required=True)
     state = fields.Selection([
         ('draft', 'Draft'),
         ('pending', 'Pending'),
-    ],default='draft')
+    ], default='draft')
     reason = fields.Char()
 
     def action_confirm(self):
-        self.property_id=self.state
+            self.grade_id.state = self.state
